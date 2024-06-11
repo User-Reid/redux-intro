9061ffc695795e90dc94fc2e273feaf670ef4a55
Reid Herrera <reidherrerabusiness@gmail.com>
2024-06-10 22:21:58 -0500

    implimented some dispatches :)

--- src/store.js
+++ src/store.js
@@ -42,20 +42,42 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/payLoan",
+//   payload: "To stay out of debt🥳",
+// });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
 
-console.log(store.getState());
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
 
-store.dispatch({
-  type: "account/payLoan",
-  payload: "To stay out of debt🥳",
-});
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(5090, "To spend on gambling"));
+store.dispatch(payLoan());
 
 console.log(store.getState());
